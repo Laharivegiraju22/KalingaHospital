f7bbbb282fa36b699eed999790ed41143983560d
Laharivegiraju22 <58691151+Laharivegiraju22@users.noreply.github.com>
2020-01-02 21:27:37 +0530

    Update DoctorNotFoundException.java

--- kalingahospital/src/main/java/com/mindtree/kalingahospital/exceptions/serviceexcption/DoctorNotFoundException.java
+++ kalingahospital/src/main/java/com/mindtree/kalingahospital/exceptions/serviceexcption/DoctorNotFoundException.java
@@ -6,22 +6,22 @@ public class DoctorNotFoundException extends KalingaHospitalServiceException {
 
 	public DoctorNotFoundException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DoctorNotFoundException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DoctorNotFoundException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DoctorNotFoundException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
